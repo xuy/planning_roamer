4e88ceba956e99c4e363fe98bbb7147e5af189bb
Eric Xu <xu.mathena@gmail.com>
2012-09-04 20:48:32 -0500

    Add a script for quick testing

---
+++ src/quick_run.sh
@@ -0,0 +1,1 @@
+./plan ../benchmarks/airport-adl/domain.pddl ../benchmarks/airport-adl/p02-airport1-p1.pddl --heuristic "hff=ff()" --heuristic "lm=cea()" --search "lazy_greedy([hff,lm], preferred=[hff,lm])"
