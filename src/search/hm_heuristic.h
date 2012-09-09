fb335ee49a8c96483078817bd3956a894cb8f79f
Eric Xu <xu.mathena@gmail.com>
2012-09-08 21:14:17 -0500

    Cleanup, rename tuple to tuple_vector because tuple becomes a type name in C++0X

--- src/search/hm_heuristic.h
+++ src/search/hm_heuristic.h
@@ -13,7 +13,8 @@
 
 using namespace std;
 
-typedef vector<pair<int, int> > tuple;
+// C++0x introduced "tuple" type.
+typedef vector<pair<int, int> > tuple_vector;
 
 class Options;
 /**
@@ -33,18 +34,18 @@ protected:
     int m;
 
     // h^m table
-    map<tuple, int> hm_table;
+    map<tuple_vector, int> hm_table;
     bool was_updated;
 
-    void init_hm_table(tuple &t);
+    void init_hm_table(tuple_vector &t);
     void update_hm_table();
-    int eval(tuple &t);
-    int update_hm_entry(tuple &t, int val);
-    void extend_tuple(tuple &t, const Operator &op);
+    int eval(tuple_vector &t);
+    int update_hm_entry(tuple_vector &t, int val);
+    void extend_tuple(tuple_vector &t, const Operator &op);
 
     // some helper methods
-    int check_tuple_in_tuple(const tuple &tup, const tuple &big_tuple);
-    void state_to_tuple(const State &state, tuple &t) {
+    int check_tuple_in_tuple(const tuple_vector &tup, const tuple_vector &big_tuple);
+    void state_to_tuple(const State &state, tuple_vector &t) {
         for (int i = 0; i < g_variable_domain.size(); i++)
             t.push_back(make_pair(i, state[i]));
     }
@@ -63,7 +64,7 @@ protected:
         return -1;
     }
 
-    void get_operator_pre(const Operator &op, tuple &t) {
+    void get_operator_pre(const Operator &op, tuple_vector &t) {
         for (int i = 0; i < op.get_prevail().size(); i++)
             t.push_back(make_pair(op.get_prevail()[i].var, op.get_prevail()[i].prev));
 
@@ -74,7 +75,7 @@ protected:
         sort(t.begin(), t.end());
     }
 
-    void get_operator_eff(const Operator &op, tuple &t) {
+    void get_operator_eff(const Operator &op, tuple_vector &t) {
         for (int i = 0; i < op.get_pre_post().size(); i++)
             t.push_back(make_pair(op.get_pre_post()[i].var, op.get_pre_post()[i].post));
 
@@ -116,27 +117,27 @@ protected:
 
 
     void generate_all_tuples() {
-        tuple t;
+        tuple_vector t;
         generate_all_tuples(0, m, t);
     }
-    void generate_all_tuples(int var, int sz, tuple &base);
+    void generate_all_tuples(int var, int sz, tuple_vector &base);
 
-    void generate_all_partial_tuple(tuple &base_tuple, vector<tuple> &res) {
-        tuple t;
+    void generate_all_partial_tuple(tuple_vector &base_tuple, vector<tuple_vector> &res) {
+        tuple_vector t;
         generate_all_partial_tuple(base_tuple, t, 0, m, res);
     }
-    void generate_all_partial_tuple(tuple &base_tuple, tuple &t, int index, int sz, vector<tuple> &res);
+    void generate_all_partial_tuple(tuple_vector &base_tuple, tuple_vector &t, int index, int sz, vector<tuple_vector> &res);
 
     void dump_table() const {
-        map<tuple, int>::const_iterator it;
+        map<tuple_vector, int>::const_iterator it;
         for (it = hm_table.begin(); it != hm_table.end(); it++) {
-            pair<tuple, int> hm_ent = *it;
+            pair<tuple_vector, int> hm_ent = *it;
             cout << "h[";
             print_tuple(hm_ent.first);
             cout << "] = " << hm_ent.second << endl;
         }
     }
-    void print_tuple(tuple &tup) const {
+    void print_tuple(tuple_vector &tup) const {
         cout << tup[0].first << "=" << tup[0].second;
         for (int i = 1; i < tup.size(); i++)
             cout << "," << tup[i].first << "=" << tup[i].second;
