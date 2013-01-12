44fc0dc414189fecd32598ec3391a23753d22643
Eric Xu <xu.mathena@gmail.com>
2013-01-12 15:34:13 -0600

    Add two debug-specific scripts to the code base.

---
+++ src/quick_run_debug.sh
@@ -0,0 +1,4 @@
+cd search
+make debug
+cd ..
+./plan-debug ../benchmarks/airport-adl/domain.pddl ../benchmarks/airport-adl/p07-airport2-p2.pddl --heuristic "hff=ff()" --heuristic "lm=cea()" --search "lazy_greedy([hff,lm], preferred=[hff,lm])"
